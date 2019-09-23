0c463418c3f7bc52749621b698bd41c63c8023a5
Long <>
2019-09-24 00:17:40 +0900

    Added the trial script for hitting twitter api. This script enables you to watch the timeline and tweet via twiiter api.

---
+++ scripts/hit_api_sample.py
@@ -0,0 +1,44 @@
+import twitter
+import config
+import sys
+import json
+
+# authorization
+CK = config.CONSUMER_KEY
+CS = config.CONSUMER_SECRET
+AT = config.ACCESS_TOKEN
+AS = config.ACCESS_TOKEN_SECRET
+
+api = twitter.Api(consumer_key=CK, consumer_secret=CS, access_token_key=AT, access_token_secret=AS)
+
+# users = api.GetFriends()
+# print([u.name for u in users])
+
+def main():
+    pass
+
+def getTimeline():
+    res = api.GetHomeTimeline()
+    print(type(res))
+    for tw in res:
+        print('==========')
+        print(tw.user.screen_name)
+        print(tw.created_at)
+        print(tw.text)
+
+def postTweet():
+    message = input(">>")
+    api.PostUpdates(status=message, continuation='cont')
+
+
+if __name__ == "__main__":
+    flag = input(r'Do you want to tweet? y/n:')
+    convert = lambda flag: True if flag == 'y' else False
+    if flag != 'y' and flag != 'n':
+        print('Invalid input')
+    else:
+        flag = convert(flag)
+    if flag is True:
+        postTweet()
+    else:
+        getTimeline()
