fac9e778fb2148116f4f9b549f5ea327c884dc99
Long <>
2019-09-24 15:04:06 +0900

    Modified the sample scripts.

--- scripts/hit_api_sample.py
+++ scripts/hit_api_sample.py
@@ -11,27 +11,12 @@ AS = config.ACCESS_TOKEN_SECRET
 
 api = twitter.Api(consumer_key=CK, consumer_secret=CS, access_token_key=AT, access_token_secret=AS)
 
-# users = api.GetFriends()
-# print([u.name for u in users])
+# api.GetUser()
+# api.DestroyStatus()
+# api.GetStatuses()
+# api.GetUserTimeline(user_id=1412344992, count=20)
 
 def main():
-    pass
-
-def getTimeline():
-    res = api.GetHomeTimeline()
-    print(type(res))
-    for tw in res:
-        print('==========')
-        print(tw.user.screen_name)
-        print(tw.created_at)
-        print(tw.text)
-
-def postTweet():
-    message = input(">>")
-    api.PostUpdates(status=message, continuation='cont')
-
-
-if __name__ == "__main__":
     flag = input(r'Do you want to tweet? y/n:')
     convert = lambda flag: True if flag == 'y' else False
     if flag != 'y' and flag != 'n':
@@ -42,3 +27,21 @@ if __name__ == "__main__":
         postTweet()
     else:
         getTimeline()
+
+def getTimeline():
+    res = api.GetHomeTimeline()
+    print(type(res))
+    for tw in res:
+        print('==========')
+        print(tw.user.screen_name)
+        print(tw.created_at)
+        print(tw.text)
+        print()
+
+def postTweet():
+    message = input(">>")
+    api.PostUpdates(status=message, continuation='cont')
+
+
+if __name__ == "__main__":
+    main()
