be1ac100e6e599b9cb67a2d7a8e6508b326109a9
Long <>
2019-09-24 15:05:42 +0900

    Added the scripts to delete my past tweets(tweeted before 2017).

---
+++ scripts/delete_tweet.py
@@ -0,0 +1,31 @@
+import twitter
+import json
+import config
+import time
+
+# authorization
+CK = config.CONSUMER_KEY
+CS = config.CONSUMER_SECRET
+AT = config.ACCESS_TOKEN
+AS = config.ACCESS_TOKEN_SECRET
+
+api = twitter.Api(consumer_key=CK, consumer_secret=CS, access_token_key=AT, access_token_secret=AS)
+
+# load tweet
+input_path = './data/mytwitter_data/{}'
+with open(input_path.format('tweet.js')) as target:
+    tw_data = json.loads(target.read()[25:])
+
+for tweet in tw_data:
+    print(tweet)
+    year_tw = int(tweet['created_at'][-4:])
+    print(year_tw)
+    if year_tw <= 2017:
+        target_id = int(tweet['id'])
+        try:
+            api.DestroyStatus(status_id=target_id)
+            time.sleep(1)
+        except twitter.error.TwitterError:
+            pass
+        
+
